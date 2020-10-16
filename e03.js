3663a8995212c3c12d8777e2ee4fcc5989efbda4
SamyKhelfa <thebboysams@gmail.com>
2020-10-16 15:09:47 +0200

    update e03

---
+++ e03.js
@@ -0,0 +1,12 @@
+function marsos(str){
+    for (let i=0; i<str.legth; i++){
+        const error = 0
+        if (str =! "SOS"){
+            error++
+        }
+    }
+
+    return error
+}
+
+console.log(marsos('SOFSOSSISSOW'))
